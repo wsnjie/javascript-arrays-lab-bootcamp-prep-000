756a6160684fd5bbc2d61c3e82faa7ab8fb29ca1
William Njie <wsnjie@gmail.com>
2018-10-24 01:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  kittens.unshift(name)
+  kittens.push(name)
   return kittens
 }
